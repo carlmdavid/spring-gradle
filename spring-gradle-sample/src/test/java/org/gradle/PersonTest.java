d6c12f3a736f69e1f97488490697b2a958f73c71
Carl <carlmichaeldavid2@gmail.com>
2016-07-06 22:16:09 -0400

    Initial

---
+++ spring-gradle-sample/src/test/java/org/gradle/PersonTest.java
@@ -0,0 +1,16 @@
+package org.gradle;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void canConstructAPersonWithAName() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
+        assertThat(person.getName(), is(equalTo("Larry")));
+    }
+}
